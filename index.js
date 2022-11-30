a9e13ed818720ebd7d089c6da1fd85478039d7db
Musyoki_Wambua <wjosephmusyoki@gmail.com>
2022-11-30 23:15:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+// split the string into an array of strings
+
+//map over the array
+//split() splits a string object into an array of strings by separating the string into substrings
+
+//concatenate (+ between strings/ variables you want to connect)
+// charAt () returns specified character from a string 
+// slice () extracts a section of a string and returns a new string
+//join () joins all elements of an array into a string 
+
+const titleCased = () => tutorials.map(tutorial => tutorial.split(" ").map(
+    word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+  )
